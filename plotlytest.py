67e772b46fe888a6f32927f0cd47ac940aa35a3d
giorgos-andreou <gioandreou1995@gmail.com>
2019-03-22 16:41:08 +0200

    pad

--- plotlytest.py
+++ plotlytest.py
@@ -2,8 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from datetime import datetime
-import pycountry
 import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
